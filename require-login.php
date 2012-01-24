ae3874fdd4494e5b96bdbc54f6ecdcceb1e0b585
pdclark <pdclark@pdclark.com>
2012-01-24 01:27:42 -0600

    Add plugin header

--- require-login.php
+++ require-login.php
@@ -1,4 +1,34 @@
 <?php
+/*
+Plugin Name: Require Login
+Description: Require users to login to view the web site, e.g., for a staging server.
+Version: 1.0
+Author: Brainstorm Media
+Author URI: http://brainstormmedia.com/ 
+*/
+
+/**
+ * Copyright (c) 2012 Your Name. All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ */
+
 
 /**
  * Modification for issues with staging server having different domain than live
